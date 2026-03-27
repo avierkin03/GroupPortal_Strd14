35c958e03d8165ea8363a03928c7123a8bb05125
Razan6578 <ngvozdik11@gmail.com>
2026-03-27 23:06:36 +0200

    Added models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Forum(models.Model):
+    comments = [
+
+    ] #TODO array of comments
+    img = models.ImageField()#TODO
+    text = models.TextField
+    title = models.CharField(max_length=20)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forums")#TODO
+    creation_data = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+
+    img = models.ImageField()#TODO
+    like_counter = models.IntegerField()
+    dislike_counter = models.IntegerField()
+    text = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forums")#TODO
+    creation_data = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+
+
