5d428d48948a9be2cb25832531d9b8c242ada86b
Timur <timurskuratovskij@gmail.com>
2026-03-28 13:50:39 +0100

    Добавил ChatRoom разные чатик   Mute Возможность мьютить юзеров        ChatIntitation Для создания чата груп

--- chat/models.py
+++ chat/models.py
@@ -20,6 +20,35 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
+
+class ChatRoom(models.Model):
+    ROOM_TYPES = (
+        ('general', 'Общий'),
+        ('problems', 'Ошибки и решения'),
+        ('memes', 'Мемы'),
+        ('info', 'Инфо (Только админы)'),
+        ('private', 'Личный чат'),
+        ('group', 'Групповой чат'),
+    )
+    name = models.CharField(max_length=255)
+    room_type = models.CharField(max_length=20, choices=ROOM_TYPES, default='general')
+    members = models.ManyToManyField(User, related_name='chat_rooms')
+
+class Mute(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='muted_by')
+    muted_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='is_muted_by')
+
+class ChatInvitation(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_invites')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_invites')
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=(('pending', 'Ожидание'), ('accepted', 'Принято'), ('declined', 'Отклонено')), default='pending')
+
+    def can_user_post(self, user):
+        if self.room_type == 'info':
+            return user.is_staff or user.groups.filter(name='Moderators').exists()
+        return True
+
     class Meta:
         ordering = ['timestamp'] # Чтобы соо шли по порядку
 
