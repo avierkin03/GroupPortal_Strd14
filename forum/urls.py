e4dc9e3de367529a2dd04803cd25e37dcdfabc15
avierkin03 <avierkin03@gmail.com>
2026-04-17 16:37:04 +0300

    added faq urls to base.html

--- forum/urls.py
+++ forum/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.ForumListView.as_view(), name="forum-list"),
     path("forum-create/", views.ForumCreateView.as_view(), name="forum-create"),
-    path("/<int:pk>/comments/", views.CommentListView.as_view(), name="forum-comments"),
-    path("/<int:pk>/comment-create/", views.CommentCreateView.as_view(), name="forum-comments-create"),
+    path("<int:pk>/comments/", views.CommentListView.as_view(), name="forum-comments"),
+    path("<int:pk>/comment-create/", views.CommentCreateView.as_view(), name="forum-comments-create"),
     path("forum-create/", views.ForumCreateView.as_view(), name="forum-create"),
     path("<int:pk>/delete/", views.ForumDeleteView.as_view(), name="forum-delete"),    
 ]
